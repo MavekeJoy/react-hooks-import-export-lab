4903dc1a1800109fd2e08d98c55da542b0ee4a59
JoyMaveke <Mavekejoy@gmail.com>
2024-07-31 19:11:31 +0300

    import export lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { username,city } from "src/data/user.js";
+import { username,city } from "../data/user.js";
 
 function Home() {
   return (
@@ -10,3 +10,4 @@ function Home() {
     </div>
   );
 }
+export default Home;
